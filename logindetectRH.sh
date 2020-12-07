7201f86b1a3848d6196c2481bba3bcefe4937bd8
Sebastian Rosario <sebastian6417@gmail.com>
2020-12-07 17:00:34 -0500

    Add files via upload

---
+++ logindetectRH.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Searches auth.log for sudo sessions, ssh logins between 12AM and 6AM, and admin logins
+awk '/sud?o?:session/ {print NR," ",$0}' /var/log/secure >> /var/log/logindetect.log
+echo -e "\n"
+awk '/sshd/ {if($3 < "06"){print NR," ",$0}}' /var/log/secure >> /var/log/logindetect.log
+echo -e "\n"
+awk '/srosario.$/ {print NR," ",$0}' /var/log/secure >> /var/log/logindetect.log
+echo -e "\n"
